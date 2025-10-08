a6c19e25940019b0c333964a9786445530bfb8e7
brandonzion <brandonzion.sun@gmail.com>
2025-10-08 11:05:13 -0400

    Update Footer component and finalize portfolio

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,8 +3,6 @@
 import { FaGithub, FaLinkedin, FaEnvelope, FaMusic } from "react-icons/fa";
 
 export default function Footer() {
-  const currentYear = new Date().getFullYear();
-
   return (
     <footer className="bg-slate-950 border-t border-amber-600/20 py-6">
       <div className="w-full flex items-center justify-center px-2 sm:px-6 lg:px-8">
